44030924b1e99b0440029f489413e994b05da545
ShadowCoffin <51836550+ShadowCoffin@users.noreply.github.com>
2021-08-14 14:40:16 -0300

    Add files via upload

---
+++ practico_01-master/Practico1/Program.cs
@@ -0,0 +1,64 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi Primera app C#");
+
+            string nombre = "";
+            string documento = "";
+            string fnacimiento = "";
+            string exit = "N";
+            List<Persona> lista = new List<Persona>();
+
+            while (exit == "N")
+            {
+                Console.WriteLine("Nombre:");
+                nombre = Console.ReadLine();
+                Console.WriteLine("Documento:");
+                documento = Console.ReadLine();
+                Console.WriteLine("Fecha Nacimiento (dd-mm-aaaa):");
+                fnacimiento = Console.ReadLine();
+
+                DateTime nacimiento = DateTime.ParseExact(fnacimiento, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                Persona p = new Persona(nombre, documento, nacimiento);
+                lista.Add(p);
+                Console.WriteLine("Exit (Y/N):");
+                exit = Console.ReadLine();
+            }
+
+            // Ordenamos la lista.
+            lista = lista.OrderByDescending(x => x.GetEdad()).ToList();
+
+            // Mapeamos a una lista de string.
+            List<string> prueba = lista.Select(x => "Nombre: " + x.Nombre + ", Documento: " +
+                    x.Documento + ", F. Nacimiento: " + x.FechaNacimiento.ToString("dd-MM-yyyy") +
+                    ", Edad: " + x.GetEdad()).ToList();
+
+            // Imprimimos
+            prueba.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            // Prueba Where
+            lista = lista.Where(x => x.Nombre == "pepe" && x.GetEdad() > 50).ToList();
+            Console.WriteLine();
+            prueba.ForEach(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ practico_01-master/Shared/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Documento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Persona(string nombre, string documento, DateTime fechaNacimiento)
+        {
+            Nombre = nombre;
+            Documento = documento;
+            FechaNacimiento = fechaNacimiento;
+        }
+
+        public int GetEdad()
+        {
+            return (DateTime.Now - FechaNacimiento).Days / 365;
+        }
+    }
+}
